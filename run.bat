08dc7b85ac7d764f8c6a1c46ebb0b65ccbd29233
kadraman <kevin.lee@microfocus.com>
2024-08-29 11:06:32 +0100

    Updating run scripts

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ set FLASK_APP=app
 set FLASK_ENV=development
 set FLASK_DEBUG=1
 REM flask --app app init-db
-flask --app app run
+flask --app app run --host 0.0.0.0
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 #flask --app app init-db
-flask --app app run
+flask --app app run --host 0.0.0.0
